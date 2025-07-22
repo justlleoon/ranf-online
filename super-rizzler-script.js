eb6cf5a6e588ec9ec5f74bebebf14447251c0183
lleoon <96005316+justlleoon@users.noreply.github.com>
2025-07-22 21:03:33 +0800

    freinferfeerf

--- super-rizzler-script.js
+++ super-rizzler-script.js
@@ -1,10 +1,22 @@
+// Assuming 'data.json' is accessible via a web server
+fetch('accounts.json.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json(); // Parses the JSON response into a JavaScript object
+  })
+  .then(data => {
+    console.log(data); // Work with the parsed JSON data
+  })
+  .catch(error => {
+    console.error('Error fetching or parsing JSON:', error);
+  });
 function onstart(){
     $("div.chc").hide();
 };
 function checkMate(){
     let bubba = document.forms["fui"]["cnid"].value;
     let ccv = document.forms["fui"]["ccv"].value;
-    alert(bubba)
-    alert(ccv)
     return false
 }
