208ab1b92086da7c9546e1785292fef522603113
lleoon <96005316+justlleoon@users.noreply.github.com>
2025-07-22 20:55:41 +0800

    dwedwe

--- super-rizzler-script.js
+++ super-rizzler-script.js
@@ -1,3 +1,9 @@
 function onstart(){
     $("div.chc").hide();
 };
+function checkMate(){
+    let bubba = document.forms["fui"]["cnid"].value;
+    let ccv = document.forms["fui"]["ccv"].value;
+    console.log(bubba)
+    console.log(ccv)
+}
